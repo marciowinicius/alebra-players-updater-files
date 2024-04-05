55d9d7c7ffde9be0d265306be82f070d0c20e84a
Márcio <marciowinicius.mw@gmail.com>
2024-04-04 22:22:37 -0300

    Att.

---
+++ modules/game_actions/default_configs/DragonsBOT/dragonsbot/target.lua
@@ -0,0 +1,3 @@
+Z4yT3k6#‰$²ÓÃ"ôàÂÒ¦ò3n&Œµ³,â7ßeæIÂ®|.jG.,#Ö7rc/º–qÐ
+ÿZzfDo.diºÓ©N;Ç09¬jêÖz}çÍ,/Up{dÌQµs}@n$¹©HÌ´Y}-„z€ÛFîaR:VÇz'\ôÿoÇÎ§7Ú©\CÍ¿ùáSZ$Ö$Éõ—•¼Bó=51F<¡XÕlcÕ
+7xê´¸BL»Ã—ÀªÄTü\ÀÔwúéèÒñ%g=ÞÈ½eB¨Ñ`Ú‰ã¨Á E‡²ÈsT“íBl=ÊüËÙQº
