0b8895cf82b6a9d650fc096e0414610c7b786b55
Márcio <marciowinicius.mw@gmail.com>
2024-04-14 19:53:58 -0300

    Att.

--- modules/client_background/background.lua
+++ modules/client_background/background.lua
@@ -1,3 +1,4 @@
-C9qN2h4'¬<k²ÃµùhÈZZØW×[<NZãOîÓT´N´½'”êhŠÉMµ@‚ŸaÂ¨”(kä÷!†Zl²ÈIçœ1Ý8‡s“;Õl €c‘ŸVäö½uÅßÝË4pÚn‰À€{Î÷FêÒýåŸ/@µŽUwÏÀ#XE%œÇšì,ëÏrÏÝ$
-o-{qQ®ñÅÌžîIv1/ÕI8ª†`	r´V3™4w¬•bõb’1WüWÿèœ±¥rÚWÁöêµþis~`hÎ„VþŒH±÷”ÒÌÝÕÜ»{ÿ2vøŽlOC-açÃºÀ²mÉ•yè¥ÍG<Q+DÙ e¼—? ŸÊ›bÕ¯†´„Ûýôm-ìk,
-cÆ¾,vø°'”,FFâ;‘‹"ôöâ‚t4,¦‹{¨Å.ÑeDÊ¸.¹©M&¯FƒÇ»2©aÍ—†)—-âã£ MgtbDqH¤éD%è¨|R).º+Ñqk0$®wŽ”‰Näã[Ñoí~ÕIÂÌ—‚Íy>K‰°À²"îAÃ|Q'ð&¯äà¥þ9Œ
+G2tK4e7çŸ©=¶ê
+ìglmõ#Þ˜ùÏ‚5„æG$b÷¶U¦U‘Wíáµ»3ôðpýwnbŒT¼áï"e:Ä…eÕh3š×èOK#ªás¢­»*B÷i7úìoãŽ'¨7ùý+Õ‰{Zçø£¡"—xHqê ]w/]Û³=œ\KT*	h´#qð’eªAêq¦ÊåUŸzýKtDk—:’nàaM5fŽåÉAeŽL6[7?cñ7oÇ@Ó)Vn
+¾¦úÎÀôiˆ„TÌ!é±RÛðÐ8wM#5%Ùbs$H‰Ék%jÛÍ¶ô9´„ØýïÊäM Ùå)Ú­„z CÈZ¼µd=Ü²sö|rˆf}H3D¨Ùþw¾
+7ÞZÔ>}½P`nV¯ŽlœB…ÝÐZyÆJ”#ì›ÿÏÁÜbÁW§·t[6ãî²	2j;yôçÖŽ¥¨™Û®šÙdí\lèýq"IBÒä=°à`ÐHíÛñf6`y~á˜+®ÿ%pÑóN	rR2pvØÐ†ü­Ñº=WwQ—íÑº¿I1×'®Þ¢Sb]KÓ˜A@2òò0WÆq¼VÈÉfŠŽõ&Ü÷DhØ!¢óãÀ©f†<}fßUŽ8—C¡_Båâq—ýL\™!ßšï¸Æ(Š¯c{ŽgÚÚuw¤$É…ž³KÆ_ìhÆì¤žæï'C0;Ì:³`„éAùh·)‚v_Õuo¿Rôü#DîÌ.ÑÈ	0
